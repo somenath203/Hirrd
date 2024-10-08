c2597a3f13c4a2bb8782ad3546b91782fc49a046
Somenath Choudhury <somenathchoudhury35@gmail.com>
2024-10-08 07:23:07 +0530

    commited

--- src/pages/OnBoarding.jsx
+++ src/pages/OnBoarding.jsx
@@ -59,11 +59,11 @@ const OnBoarding = () => {
 
   
   return (
-    <div className="flex flex-col items-center justify-center mt-32">
+    <div className="flex flex-col items-center justify-center mt-20 lg:mt-32">
 
-      <h2 className="gradient-title font-extrabold text-7xl sm:text-8xl tracking-tighter">I am a...</h2>
+      <h2 className="gradient-title font-extrabold text-6xl sm:text-8xl tracking-tighter">I am a...</h2>
 
-      <div className="mt-16 grid grid-cols-2 gap-4 w-full md:px-40">
+      <div className="mt-16 grid grid-cols-1 lg:grid-cols-2 gap-4 w-full md:px-40">
 
         <Button 
           variant='blue' 
